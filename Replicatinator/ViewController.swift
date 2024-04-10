42b04ebb49893a00cedc9c3773e63ee3f4201fd8
Philip D'Aloia <philip.daloia@gmail.com>
2024-04-09 22:37:56 -0400

    Adding initial structure + removing default storyboard

--- Replicatinator/ViewController.swift
+++ Replicatinator/ViewController.swift
@@ -8,10 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Replicatinator"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .white
+        
+        self.view.addSubview(self.titleLabel)
+        
+        NSLayoutConstraint.activate([
+            self.titleLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            self.titleLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+        ])
     }
 
 
